9301af3acb4c60b7b02da0829584173166016b4d
aaronsms <aaronsms@u.nus.edu>
2020-08-05 20:55:04 +0800

    Support compiling C programme in-memory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
+from sys import argv, stderr, stdout
+from subprocess import check_output, CalledProcessError
+from io import BytesIO
 
 from elftools.elf.elffile import ELFFile
 from elftools.dwarf.dwarfinfo import DWARFInfo
@@ -6,16 +8,55 @@ from elftools.dwarf.compileunit import CompileUnit
 from elftools.dwarf.die import DIE
 
 def main():
-    filename = sys.argv[1]
+    elf = get_elf_file()
 
-    with open(filename, 'rb') as stream:
-        elf = ELFFile(stream)
-        dwarf_info = elf.get_dwarf_info()
+    if (elf is None):
+        return
+        
 
-        for CU in dwarf_info.iter_CUs():
-            for DIE in CU.iter_DIEs():
-                if (DIE.tag == 'DW_TAG_structure_type' or DIE.tag == 'DW_TAG_member'):
-                    print(DIE)
+    dwarf_info = elf.get_dwarf_info()
+
+    for CU in dwarf_info.iter_CUs():
+        for DIE in CU.iter_DIEs():
+            if (DIE.tag == 'DW_TAG_structure_type' or DIE.tag == 'DW_TAG_member'):
+                print(DIE)
+    
+    elf.stream.close()
+
+def get_elf_file():
+    if (len(argv) > 2):
+        print('Please specify only 1 file', file=stderr)
+        return
+
+    if (len(argv) == 1):
+        print('File is not specified', file=stderr)
+        return
+
+    filename = argv[1]
+
+    try:
+        stream = _process_file(filename)
+        return ELFFile(stream)
+    except IOError:
+        print('File not found', file=stderr)
+        return
+    except CalledProcessError:
+        print('Compilation error', file=stderr)
+        return
+    except ValueError:
+        print('Filetype is not supported', file=stderr)
+        stream.close()
+        return
+
+
+
+def _process_file(filename):
+    if (filename.split('.')[-1] == 'c'):
+        output = check_output(['gcc', '-g', '-o', '/dev/stdout', filename])
+        stream = BytesIO(output)
+    else:
+        stream = open(filename, 'rb')
+    return stream
 
 if __name__ == '__main__':
     main()
